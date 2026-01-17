af3b071c1460dee089b9ab1163a7367c30f85a1c
Sigrajavascript1717 <sigra170800@gmail.com>
2026-01-17 18:45:38 +0700

    tes

--- bunga.py
+++ bunga.py
@@ -1,21 +1,20 @@
-import turtle as tur
-import colorsys as cs
+import math
+import turtle
 
-tur.setup(800,800)
-tur.speed(10)
-tur.tracer(10)
-tur.width(2)
-tur.bgcolor('black')
-r = 10
-for j in range(25):
-    for i in range(15):
-        for q in range(100):
-            tur.color(cs.hsv_to_rgb(i/15, j/25, True))
-            tur.right(90)
-            tur.circle(200-j*4,90)
-            tur.left(90)
-            tur.circle(200-j*4,90)
-            tur.right(100)
-            tur.circle(q + r,24)
-tur.hideturtle()
-tur.done
+def xt(t):
+    return 16*math.sin(t)**3
+
+def yt(t):
+    return 13*math.cos(t)-5*\
+        math.cos(2*t)-2*\
+        math.cos(3*t)-\
+        math.cos(4*t)
+
+t = turtle.Turtle()
+t.speed(500)
+turtle.bgcolor('black')
+
+for i in range (2550):
+    t.goto((xt(i)*20, yt(i)*20))
+    t.pencolor('red')
+    t.goto(0,0)
